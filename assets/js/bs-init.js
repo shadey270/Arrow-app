e8766596f49d5afab81a9e2460ce11cf72bfc9e5
shade <oladeleshadey@gmail.com>
2022-09-03 03:52:41 -0700

    Arrow-app

---
+++ assets/js/bs-init.js
@@ -0,0 +1,7 @@
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate data-bss-parallax-bg data-bss-scroll-zoom');
+}
+
+$(document).ready(function(){
+	AOS.init();
+});
